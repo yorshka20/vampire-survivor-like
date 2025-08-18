a4c9570725c0a80321a4e8f8b8f8743c014faa4b
幽儿熙卡 <mail@yors.hk>
2025-08-18 20:09:50 +0900

    fix: game will register essential systems auto

--- packages/render/src/canvas2d/create.ts
+++ packages/render/src/canvas2d/create.ts
@@ -2,8 +2,8 @@ import { IRenderer } from '@render/types/IRenderer';
 import { Canvas2dRenderer } from './Canvas2dRenderer';
 import { BackgroundRenderLayer, EntityRenderLayer, GridDebugLayer } from './layers';
 
-export function createCanvas2dRenderer(rootElement: HTMLElement): IRenderer {
-  const renderer = new Canvas2dRenderer(rootElement);
+export function createCanvas2dRenderer(rootElement: HTMLElement, name: string): IRenderer {
+  const renderer = new Canvas2dRenderer(rootElement, name);
 
   // add necessary layers
   renderer.addRenderLayer(EntityRenderLayer);
